41d24944cb7352a82cdf71cd8ae77127bc89de37
codepixelstudio <codepixel@codepixelstudio.com>
2020-12-15 15:02:00 -0700

    update homepage alert HTML

--- elements/homepage/dvm/panels/panel.billboard.php
+++ elements/homepage/dvm/panels/panel.billboard.php
@@ -41,7 +41,7 @@
             <!-- message -->
             <span class="alert_message">
 
-                <?php echo $homepage_alert[ 'alert_text' ]; ?>&nbsp;-&nbsp;
+                <?php echo $homepage_alert[ 'alert_text' ]; ?>
 
             </span>
             <!-- END message -->
--- elements/homepage/vth/vth.billboard.php
+++ elements/homepage/vth/vth.billboard.php
@@ -46,7 +46,7 @@
             <!-- message -->
             <span class="alert_message">
 
-                <?php echo $homepage_alert[ 'alert_text' ]; ?>&nbsp;-&nbsp;
+                <?php echo $homepage_alert[ 'alert_text' ]; ?>
 
             </span>
             <!-- END message -->
